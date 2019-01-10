0915955a805a0a76165fbad1a1cf5d983b5a16d7
Gregor <gregor@martynus.net>
2019-01-10 11:22:34 -0800

    fix: handle "Command failed: wmic os get Caption" error

--- index.js
+++ index.js
@@ -3,5 +3,13 @@ module.exports = getUserAgentNode
 const osName = require('os-name')
 
 function getUserAgentNode () {
-  return `Node.js/${process.version.substr(1)} (${osName()}; ${process.arch})`
+  try {
+    return `Node.js/${process.version.substr(1)} (${osName()}; ${process.arch})`
+  } catch (error) {
+    if (/wmic os get Caption/.test(error.message)) {
+      return 'Windows <version undetectable>'
+    }
+
+    throw error
+  }
 }
