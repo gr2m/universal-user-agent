b444170b38bcab56545662ff131438dcf1bf2e27
Jascha Ephraim <jaschaephraim@gmail.com>
2023-09-15 15:25:29 -0700

    support edge runtime

--- index.js
+++ index.js
@@ -3,10 +3,14 @@ export function getUserAgent() {
     return navigator.userAgent;
   }
 
-  if (typeof process === "object" && "version" in process) {
+  if (typeof process === "object" && "version" in process && process.version !== undefined) {
     return `Node.js/${process.version.substr(1)} (${process.platform}; ${
       process.arch
     })`;
+  }
+
+  if (typeof EdgeRuntime === "string") {
+    return "Edge Runtime";
   }
 
   return "<environment undetectable>";
